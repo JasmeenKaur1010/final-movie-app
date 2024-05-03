6fabe763d8bb4ea29c64663ce462767952c55aa7
jasmeen <jasmeenaujla0000@gmail.com>
2024-05-03 12:40:16 +0530

    movie app

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+// Packages
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
+
+// Files
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import genreRoutes from "./routes/genreRoutes.js";
+import moviesRoutes from "./routes/moviesRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+
+// Configuration
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+// Routes
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/genre", genreRoutes);
+app.use("/api/v1/movies", moviesRoutes);
+app.use("/api/v1/upload", uploadRoutes);
+
+const dirname = path.resolve();
+
+
+app.use("/uploads", express.static(path.join(dirname + "/uploads")));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
